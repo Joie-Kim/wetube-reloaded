08bd5d0ba479b69c53b85db899b4226e9ce3b1de
Joie-Kim <joie.huiju@gmail.com>
2022-06-06 19:03:10 +0900

    feat: set Router

---
+++ src/controllers/videoController.js
@@ -0,0 +1,6 @@
+export const trending = (req, res) => res.send('Trending videos');
+export const search = (req, res) => res.send('search video');
+export const upload = (req, res) => res.send('upload videos');
+export const see = (req, res) => res.send('see video');
+export const edit = (req, res) => res.send('Edit video');
+export const remove = (req, res) => res.send('remove video');
--- src/server.js
+++ src/server.js
@@ -1,24 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan('dev');
-
-const handleHome = (req, res) => {
-  return res.send('here is home!');
-};
-const handleProtected = (req, res) => {
-  return res.send('Welcome to private lounge');
-};
-
 app.use(logger);
-// app.use(morgan('dev'));
-app.use(privateMiddleware);
 
-app.get('/', handleHome);
-app.get('/protected', handleProtected);
+app.use('/', globalRouter);
+app.use('/videos', videoRouter);
+app.use('/users', userRouter);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
